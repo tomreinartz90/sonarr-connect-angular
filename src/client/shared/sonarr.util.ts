f40a5596d5526412ddb19f736e6c96f4212a6c1d
t.reinartz <t.reinartz@maxxton.com>
2018-03-06 12:26:29 +0100

    update sonarr util

--- src/client/shared/sonarr.util.ts
+++ src/client/shared/sonarr.util.ts
@@ -1,15 +1,14 @@
-import {StorageService} from "./storage.service";
-import {Injectable} from "@angular/core";
+import { StorageService } from "./storage.service";
+import { Injectable } from "@angular/core";
 
 @Injectable()
 export class SonarrUtil {
 
-  constructor(private storage: StorageService) {
-
+  constructor( private storage: StorageService ) {
   }
 
 
-  calculateEpisodeQuoteColor(episodeFileCount:number, totalEpisodeCount:number, monitored:boolean, status:string) {
+  calculateEpisodeQuoteColor( episodeFileCount: number, totalEpisodeCount: number, monitored: boolean, status: string ) {
     let episodeQuote = {
       'continuing': 'label regular',
       'ended': 'label success',
@@ -18,64 +17,71 @@ export class SonarrUtil {
     };
 
     let label = "";
-    if (episodeFileCount == totalEpisodeCount)
-      if (status == 'continuing')
-        label = episodeQuote['continuing'];
-      else
-        label = episodeQuote['ended'];
-    else if (monitored)
-      label = episodeQuote['missing-monitored'];
-    else
-      label = episodeQuote['missing-not-monitored'];
+    if ( episodeFileCount == totalEpisodeCount ) {
+      if ( status == 'continuing' ) {
+        label = episodeQuote[ 'continuing' ];
+      } else {
+        label = episodeQuote[ 'ended' ];
+      }
+    } else if ( monitored ) {
+      label = episodeQuote[ 'missing-monitored' ];
+    } else {
+      label = episodeQuote[ 'missing-not-monitored' ];
+    }
 
     return label;
   }
 
 //format date to be used in api
 //TODO improve
-
-  formatDate(date:Date, positiveOffset:any) {
-    if (positiveOffset != null)
-      date.setDate(date.getDate() + parseInt(positiveOffset));
+  formatDate( date: Date, positiveOffset: any ) {
+    if ( positiveOffset != null ) {
+      date.setDate( date.getDate() + parseInt( positiveOffset ) );
+    }
     return (date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + (date.getDate()));
   }
 
-  getImageUrl(data:any) {
-    if (typeof data == "object") {
-      let start = data.url.indexOf('MediaCover');
-      return this.storage.getSonarrConfig().url + "api/" + data.url.substring(start) + "&apikey=" + this.storage.getSonarrConfig().apiKey;
+  getImageUrl( data: any ) {
+    if ( typeof data == "object" ) {
+      let start = data.url.indexOf( 'MediaCover' );
+      return this.storage.getSonarrConfig().url + "api/" + data.url.substring( start ) + "&apikey=" + this.storage.getSonarrConfig().apiKey;
     } else {
       return "";
     }
   }
 
 //format episodenumbers to match scene formatting
-  formatEpisodeNumer = function (seasonNumber:number, episodeNumber:number) {
+  formatEpisodeNumer = function ( seasonNumber: number, episodeNumber: number ) {
     return "S" + (seasonNumber.toString().length === 1 ? '0' : '') + seasonNumber + "E" + (episodeNumber.toString().length === 1 ? '0' : '') + episodeNumber;
   }
 
 //comparator to sort seasons by seasonNumber
-  seasonComparator(a:any, b:any) {
-    if (a.seasonNumber < b.seasonNumber)
+  seasonComparator( a: any, b: any ) {
+    if ( a.seasonNumber < b.seasonNumber ) {
       return -1;
-    else if (a.seasonNumber > b.seasonNumber)
+    } else if ( a.seasonNumber > b.seasonNumber ) {
       return 1;
+    }
     return 0;
   }
 
 // comparator to sort seasons by seasonNumber
-  seriesComparator(a:any, b:any) {
-    if (a.status != b.status) {
-      if (a.status < b.status)
+  seriesComparator( a: any, b: any ) {
+    if ( a.status != b.status ) {
+      if ( a.status < b.status ) {
         return -1;
-      if (a.status > b.status)
+      }
+      if ( a.status > b.status ) {
         return 1;
+      }
       return 0;
     }
-    if (a.sortTitle < b.sortTitle)
+    if ( a.sortTitle < b.sortTitle ) {
       return -1;
-    if (a.sortTitle > b.sortTitle)
+    }
+    if ( a.sortTitle > b.sortTitle ) {
       return 1;
+    }
     return 0;
   }
 }
